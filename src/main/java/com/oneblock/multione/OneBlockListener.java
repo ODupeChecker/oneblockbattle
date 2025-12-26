13490016b1376de23e074c9c9c8cbd94daef79a0
LastStandzzz <106276649+ODupeChecker@users.noreply.github.com>
2025-12-26 15:27:26 +0300

    Allow block drops for tracked OneBlocks

---
+++ src/main/java/com/oneblock/multione/OneBlockListener.java
@@ -0,0 +1,23 @@
+package com.oneblock.multione;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class OneBlockListener implements Listener {
+
+    private final OneBlockManager manager;
+
+    public OneBlockListener(OneBlockManager manager) {
+        this.manager = manager;
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        OneBlock ob = manager.getOneBlock(event.getBlock().getLocation());
+        if (ob == null) {
+            return;
+        }
+        manager.handleBreak(ob);
+    }
+}
